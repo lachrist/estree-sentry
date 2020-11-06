672e73dbefec2e5f4ab18660695014b9991d56ff
Laurent Christophe <lachrist@vub.ac.be>
2020-11-06 22:31:36 +0100

    a lot of bug squashed, still cutting through acorn's test

--- test/acorn/index.js
+++ test/acorn/index.js
@@ -1,11 +1,34 @@
 const Acorn = require("acorn");
 const EstreeSentry = require("../../lib/index.js");
+const Chalk = require("chalk");
 const acorn = {
   __proto__: Acorn,
   parse: (code, options) => {
+    if (options.preserveParens) {
+      console.log("Skipping test because options.preserveParens");
+      return Acorn.parse(code, options);
+    }
     const ast = Acorn.parse(code, options);
-    const errors = EstreeSentry[options.sourceType ===  "module" ? "module" : "script"](ast);
-    console.log(JSON.stringify(errors, null, 2));
+    try {
+      const errors = EstreeSentry[options.sourceType ===  "module" ? "module" : "script"](ast);
+      if (errors.length === 0) {
+        console.log(Chalk.green("Pased"));
+      } else {
+        console.log(Chalk.yellow("SyntaxError"));
+        console.log(options);
+        console.log(code);
+        console.log(JSON.stringify(errors, null, 2));
+      }
+
+    } catch (error) {
+      if (error.name !== "SentryError") {
+        throw error;
+      }
+      console.log(Chalk.red("SentryError"));
+      console.log(options);
+      console.log(code);
+      console.log(error);
+    }
     return ast;
   }
 };
