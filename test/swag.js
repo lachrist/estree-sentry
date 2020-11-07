bb32aa5ebd952eb6024ae80775f97c3a79efcf6d
Laurent Christophe <lachrist@vub.ac.be>
2020-11-07 17:53:14 +0100

    acorn passing test are almost all green

--- test/swag.js
+++ test/swag.js
@@ -1,6 +1,6 @@
-{
-  function f () {
-    import("foo");
-  }
-  f();
-}
+// (class C extends Object { constructor() { (() => { super() }); } })
+
+
+// (class C extends Object { constructor() { function f () { super() }; } })
+const ast = require("acorn").parse(`let x = 1; x = 2`, {ecmaVersion:2020});
+console.log(require("../lib/index.js").script(ast));
